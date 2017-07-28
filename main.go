b898e12a1ebd1c81018b7f36365d591e1ce38c50
Gareth Owen <gareth.w.owen@gmail.com>
2017-07-28 23:53:46 +0100

    Well, as something to mess around with, works for me.

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 // struct containing all the properties
 // that an article should have
 type Article struct {
+	Id      int    `json:"Id"`
 	Title   string `json: "Title"`
 	Desc    string `json: "desc"`
 	Content string `json:"content"`
@@ -36,11 +37,24 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	// var1 := vars["var1"]
+	// var2 := vars["var2"]
+
+	// fmt.Println("Var 1: " + var1)
+	// fmt.Println("Var 2: " + var2)
+	fmt.Fprintf(w, "Key: "+key)
+}
+
 // routing
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	// myRouter.HandleFunc("/article/{key}/{var1}/{var2}/", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
